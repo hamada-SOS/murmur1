82fad9069674b4b45caf0421ce74afe8c92aeabc
Hamada <85646705+hamada-SOS@users.noreply.github.com>
2023-06-01 15:11:28 +0300

    commit

---
+++ app/src/main/java/com/example/murmur1/view/RecordFragemnt.kt
@@ -0,0 +1,22 @@
+package com.example.murmur1.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.murmur1.R
+
+
+class RecordFragemnt : Fragment(R.layout.fragment_record_fragemnt) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
